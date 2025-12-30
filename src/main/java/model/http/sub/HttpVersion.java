072c26aaba459dabd4c0463b1551116dcd858ddc
Hexeong <123macanic@naver.com>
2025-12-30 16:44:56 +0900

    feat: RequestStartLine의 method와 http 버전에 대해서 enum 클래스 구현
    Signed-off-by: Hexeong <123macanic@naver.com>

---
+++ src/main/java/model/http/sub/HttpVersion.java
@@ -0,0 +1,20 @@
+package model.http.sub;
+
+public enum HttpVersion {
+    HTTP_1_1("HTTP/1.1"),
+    HTTP_1_2("HTTP/1.2"); // HTTP/2 이상부터는 TLS/SSL을 활용한 암호화가 필요하기에 나중에 추가 확장시 추가
+
+    private final String version;
+
+    HttpVersion(String version) {
+        this.version = version;
+    }
+
+    public static HttpVersion findByType(String type) {
+        for (HttpVersion value : HttpVersion.values()) {
+            if (value.version.equals(type))
+                return value;
+        }
+        throw new IllegalArgumentException("Not found HttpVersion By " + type);
+    }
+}
