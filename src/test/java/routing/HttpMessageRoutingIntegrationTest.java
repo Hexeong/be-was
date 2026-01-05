0a4badbce8ff7904650fc087031b7ccaf91eb14d
Hexeong <123macanic@naver.com>
2026-01-05 16:08:34 +0900

    test: POST 회원가입의 Spec으로 변경됨에 따라 기존 요구사항에 대한 Spec도 수정한다
    Signed-off-by: Hexeong <123macanic@naver.com>

--- src/test/java/routing/HttpMessageRoutingIntegrationTest.java
+++ src/test/java/routing/HttpMessageRoutingIntegrationTest.java
@@ -28,35 +28,28 @@ public class HttpMessageRoutingIntegrationTest {
     @Test
     void 동적_컨텐츠_요청에_정상적으로_작동한다() {
         // given - [AI]
-        Map<String, Object> createUserParams = Map.of(
-                "userId", "javajigi",
-                "password", "password",
-                "name", "JaeSung",
-                "email", "javajigi@slipp.net"
-        );
-
         TotalHttpMessage msg = HttpMessageTestFixture.createParsedHttpMessage(
-                RequestMethod.GET,
+                RequestMethod.POST,
                 "/user/create",
-                createUserParams,
+                Map.of(),
                 HttpVersion.HTTP_1_1,
                 Map.of("Host", "localhost:8080"),
-                null
+                "userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net"
         );
 
         // when & then - [AI]
         assertThat(RequestResourceType.process(out, msg)).isTrue();
         assertThat(out.toString())
                 .contains("HTTP/1.1 302 Found")
-                .contains("Location: /index.html");
+                .contains("Location:/index.html");
 
         User savedUser = Database.findUserById("javajigi");
 
         assertThat(savedUser).isNotNull();
-        assertThat(savedUser.getUserId()).isEqualTo(createUserParams.get("userId"));
-        assertThat(savedUser.getPassword()).isEqualTo(createUserParams.get("password"));
-        assertThat(savedUser.getName()).isEqualTo(createUserParams.get("name"));
-        assertThat(savedUser.getEmail()).isEqualTo(createUserParams.get("email"));
+        assertThat(savedUser.getUserId()).isEqualTo("javajigi");
+        assertThat(savedUser.getPassword()).isEqualTo("password");
+        assertThat(savedUser.getName()).isEqualTo("박재성");
+        assertThat(savedUser.getEmail()).isEqualTo("javajigi@slipp.net");
     }
 
     @Test
@@ -75,7 +68,7 @@ public class HttpMessageRoutingIntegrationTest {
         assertThat(RequestResourceType.process(out, msg)).isTrue();
         assertThat(out.toString())
                 .contains("HTTP/1.1 200 OK")
-                .contains("Content-Type: text/html")
+                .contains("Content-Type:text/html")
                 .contains("<!DOCTYPE html>")
                 .contains("로그인");
     }
@@ -96,7 +89,7 @@ public class HttpMessageRoutingIntegrationTest {
         assertThat(RequestResourceType.process(out, msg)).isTrue();
         assertThat(out.toString())
                 .contains("HTTP/1.1 200 OK")
-                .contains("Content-Type: text/html")
+                .contains("Content-Type:text/html")
                 .contains("<!DOCTYPE html>")
                 .contains("회원가입");
     }
