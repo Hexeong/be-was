2606217183f4d4da3e25f37ed3deffdd916f1603
Hexeong <123macanic@naver.com>
2026-01-05 17:43:33 +0900

    test: 웹서버 5단계에 대한 Spec 정의 - POST /user/login 요청에 대해 정상적으로 Session Id 값이 담긴 쿠키를 발급한다.
    Signed-off-by: Hexeong <123macanic@naver.com>

--- src/test/java/user/UserRequestTest.java
+++ src/test/java/user/UserRequestTest.java
@@ -1,9 +1,11 @@
 package user;
 
+import db.Database;
 import fixture.HttpMessageTestFixture;
 import model.http.TotalHttpMessage;
 import model.http.sub.HttpVersion;
 import model.http.sub.RequestMethod;
+import model.user.User;
 import org.junit.jupiter.api.Test;
 import routing.TotalRouteMapping;
 
@@ -41,6 +43,7 @@ public class UserRequestTest {
     @Test
     void 회원가입_요청에_대하여_GET_요청을_실패한다() {
         // given
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
         TotalHttpMessage msg = HttpMessageTestFixture.createParsedHttpMessage(
                 RequestMethod.GET,
                 "/user/create",
@@ -54,5 +57,34 @@ public class UserRequestTest {
 
         // when & then
         assertThat(TotalRouteMapping.route(out, msg)).isFalse();
+    }
+
+    @Test
+    void 로그인_요청에_대하여_POST_요청을_성공한다() {
+        // given
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Database.addUser(new User(
+                "javajigi",
+                "password",
+                "박유성",
+                "javajigi@slipp.net"));
+
+        TotalHttpMessage msg = HttpMessageTestFixture.createParsedHttpMessage(
+                RequestMethod.POST,
+                "/user/login",
+                Map.of(),
+                HttpVersion.HTTP_1_1,
+                Map.of("Connection", "keep-alive",
+                        "Content-Length", "59",
+                        "Content-Type", "application/x-www-form-urlencoded",
+                        "Accept", "*/*"),
+                "userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net");
+
+        // when & then
+        assertThat(TotalRouteMapping.route(out, msg)).isTrue();
+        assertThat(out.toString())
+                .contains("302")
+                .contains("Found")
+                .contains("Location:/index.html");
     }
 }
