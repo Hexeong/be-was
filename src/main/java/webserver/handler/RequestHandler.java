d43cdae0b301081d3b729f1377845b1e32b8b90a
Hexeong <123macanic@naver.com>
2026-01-06 20:19:19 +0900

    refactor: Routing 호출 로직 리팩토링 - 기존 동적 -> 정적의 순차 처리 방식에서 Router에 등록되지 않은 요청은 정적으로 넘기되, 동적에서 동적 렌더링을 진행하는 방식으로 리팩토링
    Signed-off-by: Hexeong <123macanic@naver.com>

---
+++ src/main/java/webserver/handler/RequestHandler.java
@@ -0,0 +1,65 @@
+package webserver.handler;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+import business.BusinessHandler;
+import model.http.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import model.http.HttpRequest;
+import parser.http.HttpParserFacade;
+import resolver.view.ModelAndView;
+import routing.TotalRouteMapping;
+
+public class RequestHandler implements Runnable {
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+
+    private Socket connection;
+
+    public RequestHandler(Socket connectionSocket) {
+        this.connection = connectionSocket;
+    }
+
+    public void run() {
+        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
+                connection.getPort());
+
+        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            connection.setSoTimeout(5000);
+
+            while(true) {
+                try {
+                    // 1. request parsing
+                    HttpRequest req = HttpParserFacade.parse(in);
+                    HttpResponse res = new HttpResponse();
+
+                    log.debug(req.toString());
+
+                    // Handler를 가져온다. 처리 결과가 ViewName 또는 HttpResponse
+                    BusinessHandler handler = TotalRouteMapping.route(req);
+
+                    if (handler == null) {
+                        ResourceResponseHandler.handle(req, res);
+                        res.sendResponse(out);
+                        continue;
+                    }
+
+                    ModelAndView mv = handler.execute(req, res);
+
+                    // TODO:: forward에 대한 처리를 할경우, 다시 route부터 실행해야 한다.
+
+                    mv.resolve(req, res);
+
+                    res.sendResponse(out);
+                } catch (SocketTimeoutException e) {
+                    log.error(e.getMessage());
+                    break;
+                }
+            }
+        } catch (IOException | IllegalArgumentException e) {
+            log.error(e.getMessage());
+        }
+    }
+}
--- src/main/java/webserver/handler/response/ResponseHandler.java
+++
@@ -1,9 +0,0 @@
-package webserver.handler.response;
-
-import model.http.TotalHttpMessage;
-
-import java.io.OutputStream;
-
-public interface ResponseHandler {
-    boolean sendResponse(OutputStream out, TotalHttpMessage message);
-}
