3b045dfbcedf4dec7a15ad25c8926568a0e45bf9
Hexeong <123macanic@naver.com>
2026-01-08 17:54:45 +0900

    feat: 동적 HTTP 렌더링을 지원하는 Handler 구현체와 인터페이스 구현 - DynamicHttpHandler 구현 - DynamicPageHttpHandler 구현 - UserHttpHandler 구현
    Signed-off-by: Hexeong <123macanic@naver.com>

---
+++ src/main/java/handler/impl/DynamicPageHttpHandler.java
@@ -0,0 +1,67 @@
+package handler.impl;
+
+import annotation.Router;
+import annotation.RequestMapping;
+import db.SessionStorage;
+import extractor.http.CookieExtractor;
+import model.Model;
+import model.http.HttpRequest;
+import model.http.sub.RequestMethod;
+import model.user.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import resolver.view.ModelAndView;
+
+@Router
+public class DynamicPageHttpHandler implements DynamicHttpHandler {
+    private static final Logger log = LoggerFactory.getLogger(DynamicPageHttpHandler.class);
+
+    private static final String SESSION_ID_KEY = "sid";
+
+    public DynamicPageHttpHandler() {}
+
+    @RequestMapping(method = RequestMethod.GET, path = {"/", "/index.html"})
+    public ModelAndView indexPage(HttpRequest req) {
+        Model model = new Model();
+        setLoginStatus(req, model);
+        return new ModelAndView(model, "/index.html");
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = {"/registration", "/registration/index.html"})
+    public ModelAndView registrationPage(HttpRequest req) {
+        Model model = new Model();
+        setLoginStatus(req, model);
+        return new ModelAndView(model, "/registration/index.html");
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = {"/login", "/login/index.html"})
+    public ModelAndView loginPage(HttpRequest req) {
+        Model model = new Model();
+        setLoginStatus(req, model);
+        return new ModelAndView(model, "/login/index.html");
+    }
+
+    private void setLoginStatus(HttpRequest req, Model model) {
+        boolean isLoggedIn = false;
+
+        String sid = CookieExtractor.getValue(req, SESSION_ID_KEY);
+
+        if (sid != null) {
+            User user = SessionStorage.findUserBySid(sid);
+            if (user != null) {
+                log.debug("로그인 인증 성공 - User: {}, SID: {}", user.getName(), sid);
+                isLoggedIn = true;
+            } else {
+                log.debug("유효하지 않은 세션 ID (만료되었거나 조작됨) - SID: {}", sid);
+            }
+        }
+
+        if (!isLoggedIn) {
+            log.debug("비로그인 상태 또는 쿠키 없음");
+        }
+
+        // 4. 모델 설정
+        model.put("isLoggedIn", isLoggedIn);
+        model.put("isNotLoggedIn", !isLoggedIn);
+    }
+}
---
+++ src/main/java/handler/impl/UserHttpHandler.java
@@ -0,0 +1,68 @@
+package handler.impl;
+
+import annotation.Router;
+import annotation.RequestMapping;
+import db.Database;
+import db.SessionStorage;
+import extractor.http.CookieExtractor;
+import model.Model;
+import model.http.HttpRequest;
+import model.http.HttpResponse;
+import model.http.sub.RequestMethod;
+import model.user.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import resolver.view.ModelAndView;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@Router
+public class UserHttpHandler implements DynamicHttpHandler {
+    private static final Logger log = LoggerFactory.getLogger(UserHttpHandler.class);
+
+    private static final String COOKIE_HEADER_KEY = "Set-Cookie";
+    private static final String COOKIE_HEADER_PREFIX = "sid=";
+    private static final String SESSION_ID_KEY = "sid";
+    private static final String COOKIE_HEADER_SUFFIX = "; Path=/";
+
+    public UserHttpHandler() {}
+
+    @RequestMapping(method = RequestMethod.POST, path = "/user/create")
+    public ModelAndView createUser(HttpRequest req, HttpResponse res, User user) {
+
+        Database.addUser(user);
+
+        setSessionCookie(res, user);
+
+        return new ModelAndView(new Model(), "redirect:/");
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/user/login")
+    public ModelAndView login(HttpRequest req, HttpResponse res, User user) {
+
+        User findUser = Database.findUserById(user.getUserId());
+        if (findUser != null && findUser.getPassword().equals(user.getPassword())) {
+            setSessionCookie(res, findUser);
+            return new ModelAndView(null, "redirect:/");
+        }
+        return new ModelAndView(new Model(), "redirect:/login");
+    }
+
+    public ModelAndView logout(HttpRequest req, HttpResponse res) {
+        expireSessionCookie(res);
+        SessionStorage.removeSession(CookieExtractor.getValue(req, SESSION_ID_KEY));
+        return new ModelAndView(new Model(), "redirect:/");
+    }
+
+    private void setSessionCookie(HttpResponse res, User user) {
+        int randomSid = ThreadLocalRandom.current().nextInt(100000, 1000000);
+        SessionStorage.addSession(String.valueOf(randomSid), user);
+        res.headers().put(COOKIE_HEADER_KEY, COOKIE_HEADER_PREFIX + randomSid + COOKIE_HEADER_SUFFIX);
+    }
+
+    private void expireSessionCookie(HttpResponse res) {
+        // sid 값을 비우고, Max-Age=0으로 설정하여 브라우저가 즉시 삭제하게 함
+        // Set-Cookie 값을 응답으로 보낼 시 다른 쿠키는 영향 받지 않기에 괜찮음
+        res.headers().put(COOKIE_HEADER_KEY, "sid=; Path=/; Max-Age=0");
+    }
+}
