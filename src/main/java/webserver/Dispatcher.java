618a7788f059ddda9786c1cf7c7435f656baa028
Hexeong <123macanic@naver.com>
2026-01-08 18:11:29 +0900

    feat: WS와 WAS의 경계 분리 및 동적 클래스/객체 로딩 구현 - 리플랙션을 사용해 Handler, HandlerAdapter, Interceptor, ArgumentResolver 모두를 가져와 중앙집중으로 관리하도록 ApplicationContext 구현 - WS와 WAS 계층 분리를 위한 Dispatcher 구현
    Signed-off-by: Hexeong <123macanic@naver.com>

---
+++ src/main/java/webserver/Dispatcher.java
@@ -0,0 +1,50 @@
+package webserver;
+
+import handler.adapter.HandlerAdapter;
+import handler.HandlerExecutionChain;
+import model.http.HttpRequest;
+import model.http.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import resolver.view.ModelAndView;
+import webserver.handler.ResourceResponseHandler;
+
+import java.io.IOException;
+
+public class Dispatcher {
+    private static final Logger log = LoggerFactory.getLogger(Dispatcher.class);
+
+    private final ApplicationContext context;
+
+    public Dispatcher(ApplicationContext context) {
+        this.context = context;
+    }
+
+    public boolean doDispatch(HttpRequest req, HttpResponse res) throws IOException {
+        HandlerExecutionChain mappedHandler = context.getHandler(req);
+
+        if (mappedHandler == null) {
+            ResourceResponseHandler.handle(req, res);
+            return true;
+        }
+
+        HandlerAdapter ha = context.getHandlerAdapter(mappedHandler.getHandler());
+
+        if (!mappedHandler.applyPreHandle(req, res)) {
+            // Interceptor들 실행 중 false를 만나면 handle()하면 안되기에 return으로 종료
+            return false;
+        }
+
+        ModelAndView mv = ha.handle(req, res, mappedHandler.getHandler());
+
+        // TODO:: forward에 대한 처리를 할경우, 다시 route부터 실행해야 한다.
+
+        if (!mappedHandler.applyPostHandle(req, res)) {
+            return false;
+        }
+
+        mv.resolve(req, res);
+
+        return true;
+    }
+}
