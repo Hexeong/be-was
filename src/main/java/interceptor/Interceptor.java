009270e9066cdb4e8774d18550e652529610cd9e
Hexeong <123macanic@naver.com>
2026-01-09 12:07:54 +0900

    refactor: 구현된 Interceptor에 맞춰 HandlerExecutionChain에서의 호출 인자 수정 및 메서드 이름 수정 - 추가적으로 Interceptor 인터페이스의 메서드명 중복으로 인한 메서드명 수정
    Signed-off-by: Hexeong <123macanic@naver.com>

--- src/main/java/interceptor/Interceptor.java
+++ src/main/java/interceptor/Interceptor.java
@@ -4,8 +4,6 @@ import exception.CustomException;
 import model.http.HttpRequest;
 import model.http.HttpResponse;
 
-import java.io.IOException;
-
 public interface Interceptor {
     default boolean preHandle(HttpRequest req, HttpResponse res, Object handler) throws CustomException {
         return true; // 통과면 true
@@ -13,7 +11,7 @@ public interface Interceptor {
     default boolean postHandle(HttpRequest req, HttpResponse res, Object handler) throws CustomException {
         return true;
     };
-    default void triggerAfterCompletion(HttpRequest req, HttpResponse res) {
+    default void afterCompletion(HttpRequest req, HttpResponse res, Object handler) {
         // 보통 들어가는 로직은 다음과 같다.
         // 1. ThreadLocal 정리
         // 2. 리소스 반환
