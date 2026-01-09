8c774b8e5d54c130597f640272f6ed2286315c09
Hexeong <123macanic@naver.com>
2026-01-09 12:05:40 +0900

    feat: 초기 설계였던 Interceptor에 대해 구현체 LoginInspectInterceptor를 구현 - Interceptor에 대해 추상 메서드를 default 메서드로 변경 - preHandle메서드를 오버라이딩하여 인증 로직을 LoginInspectInterceptor에 구현
    Signed-off-by: Hexeong <123macanic@naver.com>

--- src/main/java/interceptor/Interceptor.java
+++ src/main/java/interceptor/Interceptor.java
@@ -1,9 +1,22 @@
 package interceptor;
 
+import exception.CustomException;
 import model.http.HttpRequest;
 import model.http.HttpResponse;
 
+import java.io.IOException;
+
 public interface Interceptor {
-    boolean intercept(HttpRequest req, HttpResponse res);
-    void triggerAfterCompletion();
+    default boolean preHandle(HttpRequest req, HttpResponse res, Object handler) throws CustomException {
+        return true; // 통과면 true
+    };
+    default boolean postHandle(HttpRequest req, HttpResponse res, Object handler) throws CustomException {
+        return true;
+    };
+    default void triggerAfterCompletion(HttpRequest req, HttpResponse res) {
+        // 보통 들어가는 로직은 다음과 같다.
+        // 1. ThreadLocal 정리
+        // 2. 리소스 반환
+        // 3. 실행 시간 로깅
+    };
 }
