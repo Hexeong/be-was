071b6deefdbe1bc16233964d1e903483f51ba74e
Hexeong <123macanic@naver.com>
2026-01-11 22:31:52 +0900

    chore: 바뀐 클래스 생성자에 맞게 테스트 코드 수정
    Signed-off-by: Hexeong <123macanic@naver.com>

--- src/test/java/user/UserRequestTest.java
+++ src/test/java/user/UserRequestTest.java
@@ -44,7 +44,7 @@ class UserRequestTest {
                         "Content-Type", "application/x-www-form-urlencoded",
                         "Accept", "*/*"),
                 "userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net");
-        HttpResponse res = new HttpResponse(out);
+        HttpResponse res = new HttpResponse(req, out);
 
         // when
         HandlerExecutionChain chain = context.getHandler(req);
@@ -103,7 +103,7 @@ class UserRequestTest {
                         "Content-Type", "application/x-www-form-urlencoded",
                         "Accept", "*/*"),
                 "userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net");
-        HttpResponse res = new HttpResponse(out);
+        HttpResponse res = new HttpResponse(req, out);
 
         // when
         HandlerExecutionChain chain = context.getHandler(req);
