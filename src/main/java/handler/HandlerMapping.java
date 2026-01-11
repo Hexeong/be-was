0309a104abe41451d63eaae51a78d29b76f488f1
Hexeong <123macanic@naver.com>
2026-01-11 22:34:41 +0900

    refactor: 405 처리를 위해 기존 HandlerMapping 구조를 이중 Map 구조로 변경
    Signed-off-by: Hexeong <123macanic@naver.com>

--- src/main/java/handler/HandlerMapping.java
+++ src/main/java/handler/HandlerMapping.java
@@ -1,20 +1,35 @@
 package handler;
 
+import exception.CustomException;
+import exception.ErrorCode;
+import model.http.sub.RequestMethod;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class HandlerMapping {
-    private Map<RouteKey, HandlerMethod> mapping;
+    private Map<String, Map<RequestMethod, HandlerMethod>> mapping;
 
     public HandlerMapping() {
         mapping = new HashMap<>();
     }
 
     public void addMapping(RouteKey key, HandlerMethod handler) {
-        mapping.put(key, handler);
+        Map<RequestMethod, HandlerMethod> methodMap = mapping.computeIfAbsent(key.urlPath(), k -> new HashMap<>());
+        methodMap.put(key.method(), handler);
     }
 
     public HandlerMethod getHandler(RouteKey key) {
-        return mapping.get(key);
+        Map<RequestMethod, HandlerMethod> methodMap = mapping.getOrDefault(key.urlPath(), null);
+
+        if (methodMap == null)
+            return null;
+
+        HandlerMethod handler = methodMap.getOrDefault(key.method(), null);
+
+        if (handler == null)
+            throw new CustomException(ErrorCode.METHOD_NOT_ALLOWED);
+
+        return handler;
     }
 }
