1bc1da6b35e0dba4eec03c0a308d10208047ecbd
Hexeong <123macanic@naver.com>
2026-01-11 22:34:55 +0900

    feat: 405 에러 코드 추가
    Signed-off-by: Hexeong <123macanic@naver.com>

--- src/main/java/exception/ErrorCode.java
+++ src/main/java/exception/ErrorCode.java
@@ -7,8 +7,9 @@ public enum ErrorCode {
     CANNOT_ADAPT(HttpStatus.BAD_REQUEST, "해당 핸들러를 처리할 수 있는 어댑터가 존재하지 않습니다."),
     CLASS_SCAN_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "해당 위치에서의 클래스 스캔 오류"),
 
-    // Business
-    NOT_AUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED, "인증이 필요한 곳에 접근중입니다");
+    // Application
+    NOT_AUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED, "인증이 필요한 곳에 접근중입니다"),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "해당 경로에 대해 요청 메서드를 지원하지 않습니다");
 
     private final HttpStatus status;
     private final String message;
