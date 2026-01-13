a97f1cde505d838b1c115268f40b9c1d1bb5e0af
Hexeong <123macanic@naver.com>
2026-01-14 00:29:57 +0900

    refactor: BAD_REQUEST 에러코드 추가
    Signed-off-by: Hexeong <123macanic@naver.com>

--- src/main/java/exception/ErrorCode.java
+++ src/main/java/exception/ErrorCode.java
@@ -13,7 +13,8 @@ public enum ErrorCode {
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "해당 경로에 대해 요청 메서드를 지원하지 않습니다"),
     NOT_FOUND(HttpStatus.NOT_FOUND, "해당 페이지를 찾을 수 없습니다."),
     AUTHENTICATION_FAILED(HttpStatus.FOUND, "인증에 실패하였습니다."),
-    NO_ARTICLE_DATA(HttpStatus.NOT_FOUND, "Article 데이터가 없습니다.");
+    NO_ARTICLE_DATA(HttpStatus.NOT_FOUND, "Article 데이터가 없습니다."),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "요청 메서드와 URL은 같지만, 잘못된 방식으로 요청하였습니다.");
 
     private final HttpStatus status;
     private final String message;
