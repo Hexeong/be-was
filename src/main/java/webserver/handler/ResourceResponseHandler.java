951202ab109e02282b639fae6b343cb3d2ae142c
Hexeong <123macanic@naver.com>
2026-01-13 17:59:34 +0900

    refactor: url 매핑되지 않은 html 요청에 대해 차단하도록 수정 - nio가 아닌 io를 쓰도록 변경
    Signed-off-by: Hexeong <123macanic@naver.com>

--- src/main/java/webserver/handler/ResourceResponseHandler.java
+++ src/main/java/webserver/handler/ResourceResponseHandler.java
@@ -8,38 +8,47 @@ import model.http.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class ResourceResponseHandler {
     private static final Logger log = LoggerFactory.getLogger(ResourceResponseHandler.class);
 
     private static final String CONTENT_TYPE_HEADER_KEY = "Content-Type";
 
+    // File 클래스는 상대 경로 사용 시 실행 위치 기준이므로 ./ 로 시작하는 것이 명확합니다.
     private static final String TEMPLATE_ROOT = "./src/main/resources/static";
 
     public static void handle(HttpRequest req, HttpResponse res) throws IOException {
-        if (!StaticResourceType.isStaticResourceByUrl(req.line().getPathUrl())) {
-            req.line().addIndexHtml();
+        String pathUrl = req.line().getPathUrl();
+
+        if (!StaticResourceType.isStaticResourceByUrl(pathUrl)) {
+            log.error("존재하지 않는 URL 매핑 또는 자원 확장자에 접근!");
+            throw new CustomException(ErrorCode.NOT_FOUND);
         }
 
-        if (req.line().getPathUrl().contains("..")) {
+        if (pathUrl.contains("..")) {
             log.error("이전 폴더 접근 문법 사용!");
             throw new CustomException(ErrorCode.NOT_FOUND);
         }
 
-        Path path = Paths.get(TEMPLATE_ROOT, req.line().getPathUrl());
-        if (!Files.exists(path)) {
-            log.error("파일 없음! " + path);
+        File file = new File(TEMPLATE_ROOT, pathUrl);
+
+        if (!file.exists() || !file.isFile()) {
+            log.error("파일 없음! " + file.getAbsolutePath());
             throw new CustomException(ErrorCode.NOT_FOUND);
         }
 
-        String fileExtension = FileTypeExtractor.extract(req.line().getPathUrl());
+        String fileExtension = FileTypeExtractor.extract(pathUrl);
         res.headers().put(CONTENT_TYPE_HEADER_KEY, StaticResourceType.findByType(fileExtension).getContentType());
 
+        byte[] body = new byte[(int) file.length()];
 
-        res.setBody(Files.readAllBytes(path));
+        try (FileInputStream fis = new FileInputStream(file)) {
+            fis.read(body);
+        }
+
+        res.setBody(body);
     }
 }
